2dce049b8f44608a9d1deff7187703e87e0af4e2
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-08 21:50:57 +0330

    Step-by-Step Implementation of our First Jasmine Specification

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,16 +1,30 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
 describe("CalculatorService", () => {
   // SPECIFICATION
   it("should add two numbers", () => {
     // Test is not ready to get executed we use pending()
-    pending();
+    // pending();
+
+    const calculator = new CalculatorService(new LoggerService());
+
+    const result = calculator.add(2, 2);
+
+    expect(result).toBe(4);
   });
 
   // SPECIFICATION
   it("should subtract two numbers", () => {
     // Test is not ready to get executed we use pending()
     // pending();
-
     // To Simulate a failing text
-    fail();
+    // fail();.
+
+    const calculator = new CalculatorService(new LoggerService());
+
+    const result = calculator.subtract(2, 2);
+
+    expect(result).toBe(0, "unexpected subtarction result");
   });
 });
