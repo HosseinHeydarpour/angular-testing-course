e835111d379880b256475c9bcc088522d72cd813
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-08 17:55:38 +0330

    Quick Introduction to Jasmine - Test Suites and Specifications

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,16 @@
+describe("CalculatorService", () => {
+  // SPECIFICATION
+  it("should add two numbers", () => {
+    // Test is not ready to get executed we use pending()
+    pending();
+  });
+
+  // SPECIFICATION
+  it("should subtract two numbers", () => {
+    // Test is not ready to get executed we use pending()
+    // pending();
+
+    // To Simulate a failing text
+    fail();
+  });
+});
